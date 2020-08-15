3348c003b2569387f79d67e84e377105b75d1de7
tobyhardcastle <69285345+tobyhardcastle@users.noreply.github.com>
2020-08-15 12:35:08 +1000

    Cleaned up C(YXWZ)
    Removed redundant arguments and vectorised internal sums

--- toby/cyxwz.m
+++ toby/cyxwz.m
@@ -1,25 +1,14 @@
-function c = cyxwz(y,x,w,z,T,lambda,phim,phip)
-    alpha = (1-lambda.^2).*exp(1i*(phip*z+phim*w)).*(lambda./2).^T*sqrt(factorial(y)*factorial(z)*factorial(w)*factorial(x));
+function c = cyxwz(y,x,w,z,lambda,phi)
+    % phi minus ignored, phi = phim + phip
+    T = y + w;
+    external = (1-lambda.^2).*(lambda./2).^T*sqrt(factorial(y)*factorial(z)*factorial(w)*factorial(x));
     psi = 0;
     for k = 0:T
-        %runk = k
-        chi = 0;
-        tkfact = factorial(T-k);
-        for l = max(k-y,0):min(k,w)
-            %runl = l
-            epsilon = 0;
-            wlfact = factorial(w-l);
-            for n = max(k-x,0):min(k,z)
-                %runn = n
-                znfact = factorial(z-n);
-                beta = 1i.^(w+z+2.*k-2.*l-2.*n);
-                gamma = exp(-1i.*(phip+phim).*k);
-                delta = (tkfact./wlfact).*(1./factorial(abs(T-k-w+l))).*(tkfact./znfact).*(1./factorial(abs(T-k-z+n))).*nchoosek(k,l).*nchoosek(k,n);
-                epsilon = epsilon + beta.*gamma.*delta;
-            end
-            chi = chi+epsilon;
-        end
-        psi = psi+(factorial(T-k).*factorial(k)).^(-1).*chi;
+        [l,n] = meshgrid(max(k-y,0):min(k,w),max(k-x,0):min(k,z));
+        comp_phase = 1i.^(w+z+2.*k-2.*l-2.*n).*exp(-1i.*(phi).*k);
+        chooses = bincof(T-k,w-l).*(bincof(T-k,z-n)).*bincof(k,l).*bincof(k,n);
+        epsilon = comp_phase.*chooses;
+        psi = psi + sum(sum(epsilon))./(factorial(T-k).*factorial(k));
     end
-    c = abs(alpha.*psi).^2;
+    c = abs(external.*psi).^2;
 end
