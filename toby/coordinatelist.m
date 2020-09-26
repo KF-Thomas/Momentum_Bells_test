f5ec1054f431717418bdbf287ea2ef27354c15c8
tobyhardcastle <69285345+tobyhardcastle@users.noreply.github.com>
2020-09-26 18:33:32 +1000

    Toby Update

--- toby/coordinatelist.m
+++ toby/coordinatelist.m
@@ -1,9 +1,9 @@
 % Returns a cell array with each cell containing the list of detected
 % particle coordinates for each simulated experimental run.
 %% Function:
-function colist_test = coordinatelist(lambda,phi,Tmax,cycles,quantum_efficiency,dark_rate,blur)
+function colist_test = coordinatelist(lambda,phi,Tmax,cycles,quantum_efficiency,dark_rate,blur,R,phys,Y_bias,X_bias)
     % Call the divided probability list from problist.m
-    [p,event,~] = problist(lambda,phi,Tmax);
+    [p,event] = problist(lambda,phi,Tmax,phys,Y_bias,X_bias);
     % Lists of each event for each trial:
     ys = zeros(cycles,1); xs = ys; ws = ys; zs = ys;
     % Find what event each trial corresponds to and note where these events
@@ -38,35 +38,47 @@ function colist_test = coordinatelist(lambda,phi,Tmax,cycles,quantum_efficiency,
     darkXs = randi(1E8,cycles,1)<thresh;
     darkWs = randi(1E8,cycles,1)<thresh;
     darkZs = randi(1E8,cycles,1)<thresh;
-    detectedYs = survivedYs + darkYs;
-    detectedXs = survivedXs + darkXs;
-    detectedWs = survivedWs + darkWs;
-    detectedZs = survivedZs + darkZs;
+    detectedYs = ys + darkYs;
+    detectedXs = xs + darkXs;
+    detectedWs = ws + darkWs;
+    detectedZs = zs + darkZs;
 
     %% Place detected particles at a position:
-    Ycoord = [1 1 0];
-    Xcoord = [-1 1 0];
-    Wcoord = [1 -1 0];
-    Zcoord = [-1 -1 0];
-
-    colist_test = cell(1,cycles);
-
-    % Blur factor:
+    
+    
+    % Blur factor - Y detections are mapped to somewhere in the Y-quadrant
+    % of the sphere but blurred.  This blurring can map a Y detection to an
+    % X detection etc.
+    
     sigma = blur;
+    colist_test = cell(1,cycles);
+    
     for i = 1:cycles
         cordlist_temp = [];
         for j=1:detectedYs(i)
-            cordlist_temp = [cordlist_temp; Ycoord+sigma*randn(1,3)];
-        end
-        for j=1:detectedXs(i)
-            cordlist_temp = [cordlist_temp; Xcoord+ sigma*randn(1,3)];
+            theta = pi*rand;
+            azim = pi*rand;
+            cordlist_temp = [cordlist_temp; [R*sin(theta)*cos(azim) R*sin(theta)*sin(azim) R*(1+cos(theta))] + sigma*randn(1,3)];
         end
         for j=1:detectedWs(i)
-            cordlist_temp = [cordlist_temp; Wcoord+ sigma*randn(1,3)];
+            theta = pi*rand;
+            azim = pi*rand;
+            cordlist_temp = [cordlist_temp; [R*sin(theta)*cos(azim) R*sin(theta)*sin(azim) R*(cos(theta)-1)] + sigma*randn(1,3)];
+        end
+        for j=1:detectedXs(i)
+            theta = pi*rand;
+            azim = pi*rand;
+            cordlist_temp = [cordlist_temp; [-R*sin(theta)*cos(azim) -R*sin(theta)*sin(azim) R*(1-cos(theta))] + sigma*randn(1,3)];
         end
         for j=1:detectedZs(i)
-                cordlist_temp = [cordlist_temp; Zcoord+ sigma*randn(1,3)];
+            theta = pi*rand;
+            azim = pi*rand;
+            cordlist_temp = [cordlist_temp; [-R*sin(theta)*cos(azim) -R*sin(theta)*sin(azim) -R*(1+cos(theta))] + sigma*randn(1,3)];
         end
-        colist_test{i} = cordlist_temp;
+        %% Quantum Efficiency:
+        % Cull data, keeping only the efficiency rating:
+        thresh = quantum_efficiency +.01;
+        keep_list = randi(100,size(cordlist_temp,1),1)<thresh;
+        colist_test{i} = cordlist_temp.*keep_list;
     end
 end
--- toby/problist.m
+++ toby/problist.m
@@ -4,7 +4,7 @@
 % that state.  Generating a random number will sit inside one of these
 % sections and hence correspond to a specific detection state.
 %% Function:
-function [p,event,cap] = problist(lambda,phi,Tmax)
+function [p,event] = problist(lambda,phi,Tmax,phys,Y_bias,X_bias)
     cap = 2+7*(Tmax)/3 +2*Tmax^2+2*Tmax^3/3; % The number of possible states given 
     problist = zeros(1,cap+1);
     YXWZ = zeros(cap,4);
@@ -13,7 +13,7 @@ function [p,event,cap] = problist(lambda,phi,Tmax)
         for B = 0:Tmax
             for C = max(B-A,0):min(Tmax,Tmax+B-A)
                 index = index+1;
-                problist(index) = problist(index-1)+cyxwz(A,B,C,A+C-B,lambda,phi);
+                problist(index) = problist(index-1)+cyxwz(A,B,C,A+C-B,lambda,phi,phys,Y_bias,X_bias);
                 YXWZ(index-1,:) = [A B C A+C-B];
             end
         end
